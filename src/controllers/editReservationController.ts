0a13d2c1c92e0ff2730b86f9487ab87795a100fc
m790101 <m790101@hotmail.com>
2024-09-22 13:07:48 +0800

    add edit reservation feature

---
+++ src/controllers/editReservationController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import Room from "../../models/room.model";
+import TimeSlot from "../../models/timeSlot.model";
+import Reservation from "../../models/reservation.model";
+import Treatment from "../../models/treatment.model";
+import sequelize from "../../service/db.service";
+import errorHandle from "../utils/errorHandler";
+import { ErrorCode } from "../utils/errorCode.enum";
+
+export default async function editReservationController(
+  req: Request,
+  res: Response
+) {
+  try {
+    const data = req.body;
+
+    const result = await sequelize.transaction(async (t: any) => {
+      const roomInstance = await Room.findOne({ where: { name: data.room } });
+
+      if (!roomInstance) {
+        const errorObject = errorHandle(ErrorCode.E002, "資料有誤，新增失敗");
+        return res.status(200).send(errorObject);
+      }
+
+      const timeSlot = await TimeSlot.findOne({
+        where: { startTime: data.timeSlot },
+      });
+
+      if (!timeSlot) {
+        const errorObject = errorHandle(ErrorCode.E002, "資料有誤，新增失敗");
+        return res.status(200).send(errorObject);
+      }
+
+      const treatment = await Treatment.findOne({
+        where: { value: data.treatment },
+      });
+
+      if (!treatment) {
+        const errorObject = errorHandle(ErrorCode.E002, "資料有誤，新增失敗");
+        return res.status(200).send(errorObject);
+      }
+      const newReservation = await Reservation.update(
+        {
+          treatment_id: treatment.id,
+          name: data.name,
+          equipment: data.equipment,
+        },
+        {
+          where: {
+            date: data.date,
+            room_id: roomInstance.id,
+            time_slot_id: timeSlot.id,
+          },
+        }
+      );
+
+      return newReservation;
+    });
+    res.status(200).send(result);
+  } catch (error: any) {
+    console.log(error)
+    const errorObject = errorHandle(ErrorCode.E999, error);
+    res.status(500).send(errorObject);
+  }
+}
--- src/controllers/routes/reservation.ts
+++ src/controllers/routes/reservation.ts
@@ -1,5 +1,6 @@
 import addReservationController from "../addReservationController";
 import cancelReservationController from "../cancelReservationController";
+import editReservationController from "../editReservationController";
 import getReservationController from "../getReservationController";
 
 const { Router } = require("express");
@@ -7,6 +8,7 @@ const { Router } = require("express");
 const reservationRouter = new Router();
 
 reservationRouter.post("/api/v1/reservation", addReservationController)
+reservationRouter.post("/api/v1/edit", editReservationController)
 reservationRouter.post("/api/v1/reservation/delete", cancelReservationController)
 reservationRouter.get("/api/v1/reservation/:date", getReservationController)
 reservationRouter.get("/api/v1/reservation", getReservationController)
