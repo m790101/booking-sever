fb91bc515cdee1816eb7ec3874cde49404744b28
m790101 <m790101@hotmail.com>
2024-09-24 16:51:21 +0800

    recover db file setting

--- service/db.service.ts
+++ service/db.service.ts
@@ -2,16 +2,15 @@ const { Sequelize } = require('sequelize');
 import dotenv from "dotenv";
 import pg from "pg"
 dotenv.config();
-const sequelize = new Sequelize('postgres://haowencheng:@localhost:5432/booking?sslmode=disable') // Example for postgres
+// const sequelize = new Sequelize('postgres://haowencheng:@localhost:5432/booking?sslmode=disable') // Example for postgres
 
-// const sequelize = new Sequelize({
-//   dialect: 'postgres',
-//   database:'postgres',
-//   username:'postgres.vwsolxixsbjncvaztcyk',
-//   password:process.env.DB_PASSWORD,
-//   dialectModule: pg,
-//   host: 'aws-0-ap-southeast-1.pooler.supabase.com',
-//   port:6543,
-// });
-
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    database:'postgres',
+    username:'postgres.vwsolxixsbjncvaztcyk',
+    password:process.env.DB_PASSWORD,
+    dialectModule: pg,
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    port:6543,
+  });
 export default sequelize
